305a02fc00efb38f3c5a0eb1b6e4b475478d23cb
skycat04323 <93493366+skycat04323@users.noreply.github.com>
2021-11-03 10:19:56 -0700

    Finished up unit 5 lesson 1

---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+
+        transform.position = new Vector3(Random.Range(-4,4), -6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
