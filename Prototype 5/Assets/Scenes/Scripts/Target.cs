aee7c92eedf3065bd7fc6ffaf94a233755604388
skycat04323 <93493366+skycat04323@users.noreply.github.com>
2021-11-04 10:16:26 -0700

    On 5.2 L3

--- Prototype 5/Assets/Scenes/Scripts/Target.cs
+++ Prototype 5/Assets/Scenes/Scripts/Target.cs
@@ -9,7 +9,7 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -3;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +27,17 @@ public class Target : MonoBehaviour
     {
         
     }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject); 
+    }
+
 
     Vector3 RandomForce()
     {
